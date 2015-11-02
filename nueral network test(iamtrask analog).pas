a0fbb73ee523e2c105da0bbfe1e9c252ab20b8b2
AussieMCer <andrei.okhrimouk@gmail.com>
2015-11-02 19:39:43 +0300

    Doing 2D arrays instead
    Decided to use 2 dimensional array instead of 1 dimensional ones like
    iamtrask done.

--- nueral network test(iamtrask analog).pas
+++ nueral network test(iamtrask analog).pas
@@ -1,8 +1,9 @@
 program SingleInputTest;
 
 var
-x : array of integer;
-y, l0, l1, l1_error, l1_delta : byte;
+x : array[1..4, 1..3] of byte:=((0, 0, 1), (0, 1, 1), (1, 0, 1), (1, 1, 1));
+y : array[1..4, 1..1] of byte:=((0), (0), (1), (1));
+l0, l1, l1_error, l1_delta : byte;
 i, syn0, syn1 : integer;
 
 function nonlin(x : real; deriv : boolean): real;
@@ -13,7 +14,4 @@ end;
 begin
 //writeln(nonlin(1, False));
 
-//Giving inout and output parameters
-x:=new integer[3](0,1,1);
-y:=1;
 end.
