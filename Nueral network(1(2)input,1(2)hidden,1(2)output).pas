bdcc1a4c96ad2b4498aab6956b43224221ea0100
AussieMCer <andrei.okhrimouk@gmail.com>
2015-11-07 18:48:00 +0300

    Updates
    Made the variables easier to read.

--- Nueral network(1(2)input,1(2)hidden,1(2)output).pas
+++ Nueral network(1(2)input,1(2)hidden,1(2)output).pas
@@ -1,6 +1,13 @@
 program in2h12iou2;
 
-var w1, w2, w3, w4, w5, w6, w7, w8, b1, b2, i1, i2, h1, h2, o1, o2, h1net, h2net, o1net, o2net, oh1, oh2, ooh1, ooh2, E1, E2, Et:real;
+var 
+w1, w2, w3, w4, w5, w6, w7, w8, 
+b1, b2, 
+i1, i2, 
+h1, h2, 
+ot1, ot2, o1r, o2r, 
+h1net, h2net,
+E1, E2, Et, Em1, Em2:real;
 
 //The logistics function(sigmoid function)
 function logisticsFunction(x:real):real;
@@ -14,18 +21,23 @@ begin
 w1:=0.15;w2:=0.2;w3:=0.25;w4:=0.3;w5:=0.4;w6:=0.45;w7:=0.5;w8:=0.55;
 b1:=0.35;b2:=0.6;
 i1:=0.05;i2:=0.1;
-o1:=0.01;o2:=0.99;
+ot1:=0.01;ot2:=0.99;
 
 h1net:=w1 * i1 + w2 * i2 + b1 * 1;
-oh1:=(logisticsFunction(h1net));
+h1:=(logisticsFunction(h1net));
+
 h2net:=w3 * i1 + w4 * i2 + b1 * 1;
-oh2:=(logisticsFunction(h2net));
-o1net:=w5 * oh1 + w6 * oh2 + b2 * 1;
-ooh1:=(logisticsFunction(o1net));
-o2net:=w7 * oh1 + w8 * oh2 + b2 * 1;
-ooh2:=(logisticsFunction(o2net));
-E1:=(sqr(o1 - ooh1)/2);
-E2:=(sqr(o2 - ooh2)/2);
+h2:=(logisticsFunction(h2net));
+
+o1r:=w5 * h1 + w6 * h2 + b2 * 1;
+o1r:=(logisticsFunction(o1r));
+o2r:=w7 * h1 + w8 * h2 + b2 * 1;
+o2r:=(logisticsFunction(o2r));
+
+E1:=(sqr(ot1 - o1r)/2);
+E2:=(sqr(ot2 - o2r)/2);
 Et:=E1 + E2;
-writeln(Et);
+Em1:=(-(ot1 - o1r));
+Em2:=(-(ot2 - o2r));
+writeln(Em1);
 end.
