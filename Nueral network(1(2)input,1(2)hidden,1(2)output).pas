b31a6c0075b21e53214524354a3bcff730607b6b
AussieMCer <andrei.okhrimouk@gmail.com>
2015-11-08 08:55:21 +0300

    Name changers

--- Nueral network(1(2)input,1(2)hidden,1(2)output).pas
+++ Nueral network(1(2)input,1(2)hidden,1(2)output).pas
@@ -5,9 +5,9 @@ w1, w2, w3, w4, w5, w6, w7, w8,
 b1, b2, 
 i1, i2, 
 h1, h2, 
-ot1, ot2, o1r, o2r, 
+outA1, outA2, o1r, o2r, Outo1r, Outo2r, 
 h1net, h2net,
-E1, E2, Et, Em1, Em2:real;
+E1, E2, dEtotal, dEerror1, dEerror2:real;
 
 //The logistics function(sigmoid function)
 function logisticsFunction(x:real):real;
@@ -21,8 +21,9 @@ begin
 w1:=0.15;w2:=0.2;w3:=0.25;w4:=0.3;w5:=0.4;w6:=0.45;w7:=0.5;w8:=0.55;
 b1:=0.35;b2:=0.6;
 i1:=0.05;i2:=0.1;
-ot1:=0.01;ot2:=0.99;
+outA1:=0.01;outA2:=0.99;
 
+//Forward propagation
 h1net:=w1 * i1 + w2 * i2 + b1 * 1;
 h1:=(logisticsFunction(h1net));
 
@@ -30,14 +31,17 @@ h2net:=w3 * i1 + w4 * i2 + b1 * 1;
 h2:=(logisticsFunction(h2net));
 
 o1r:=w5 * h1 + w6 * h2 + b2 * 1;
-o1r:=(logisticsFunction(o1r));
-o2r:=w7 * h1 + w8 * h2 + b2 * 1;
-o2r:=(logisticsFunction(o2r));
+Outo1r:=(logisticsFunction(o1r));
 
-E1:=(sqr(ot1 - o1r)/2);
-E2:=(sqr(ot2 - o2r)/2);
-Et:=E1 + E2;
-Em1:=(-(ot1 - o1r));
-Em2:=(-(ot2 - o2r));
-writeln(Em1);
+o2r:=w7 * h1 + w8 * h2 + b2 * 1;
+Outo2r:=(logisticsFunction(o2r));
+
+//Error calculation
+E1:=(sqr(outA1 - Outo1r)/2);
+E2:=(sqr(outA2 - Outo2r)/2);
+dEtotal:=E1 + E2;
+dEerror1:=(-(outA1 - Outo1r));
+dEerror2:=(-(outA2 - Outo2r));
+
+
 end.
