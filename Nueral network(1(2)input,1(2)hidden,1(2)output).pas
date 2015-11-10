e49f8c44ca04e562db0e17d4b53c19011d0811af
AussieMCer <andrei.okhrimouk@gmail.com>
2015-11-10 21:59:44 +0300

    Finished!!!
    Changed some variables names, completed the back propagation and put the
    whole thing in a for loop.

--- Nueral network(1(2)input,1(2)hidden,1(2)output).pas
+++ Nueral network(1(2)input,1(2)hidden,1(2)output).pas
@@ -1,14 +1,16 @@
 program in2h12iou2;
 
 var 
+a,b,
 w1, w2, w3, w4, w5, w6, w7, w8,
 w1u, w2u, w3u, w4u, w5u, w6u, w7u, w8u,
 b1, b2, 
 i1, i2, 
-h1, h2, 
+h1out, h2out, 
 outA1, outA2, o1r, o2r, Outo1r, Outo2r, 
 h1net, h2net,
 dE1, dE2, dEtotal, dEerror1, dEerror2:real;
+i:integer;
 
 //The logistics function(sigmoid function)
 function logisticsFunction(x:real):real;
@@ -24,18 +26,22 @@ b1:=0.35;b2:=0.6;
 i1:=0.05;i2:=0.1;
 outA1:=0.01;outA2:=0.99;
 
+for i:=1 to 10000 do
+begin
 //Forward propagation
 h1net:=w1 * i1 + w2 * i2 + b1 * 1;
-h1:=(logisticsFunction(h1net));
+h1out:=(logisticsFunction(h1net));
 
 h2net:=w3 * i1 + w4 * i2 + b1 * 1;
-h2:=(logisticsFunction(h2net));
+h2out:=(logisticsFunction(h2net));
 
-o1r:=w5 * h1 + w6 * h2 + b2 * 1;
+o1r:=w5 * h1out + w6 * h2out + b2 * 1;
 Outo1r:=(logisticsFunction(o1r));
 
-o2r:=w7 * h1 + w8 * h2 + b2 * 1;
+o2r:=w7 * h1out + w8 * h2out + b2 * 1;
 Outo2r:=(logisticsFunction(o2r));
+
+writeln(Outo1r,' ',Outo2r);
 
 //Error calculation
 dE1:=(sqr(outA1 - Outo1r)/2);
@@ -43,9 +49,37 @@ dE2:=(sqr(outA2 - Outo2r)/2);
 dEtotal:=dE1 + dE2;
 dEerror1:=(-(outA1 - Outo1r));
 dEerror2:=(-(outA2 - Outo2r));
-w5u:=w5-0.4*(dEerror1*(Outo1r*(1-Outo1r))*(1*h1*power(w5,(1-1))+0+0));
-writeln(w5u);
-w6u:=w6-0.4*(dEerror1*(outo2r*(1-Outo2r))*(1*h2*power(w6,(1-1))+0+0));
-writeln(w6u);
+w5u:=w5-0.4*(dEerror1*(Outo1r*(1-Outo1r))*(1*h1out*power(w5,(1-1))+0+0));
+//writeln(w5u);
+w6u:=w6-0.4*(dEerror1*(Outo2r*(1-Outo2r))*(1*h2out*power(w6,(1-1))+0+0));
+//writeln(w6u);
+w7u:=w7-0.4*(dEerror2*(Outo2r*(1-Outo2r))*(1*h2out*power(w7,(1-1))+0+0));
+w8u:=w8-0.4*(dEerror2*(Outo2r*(1-Outo2r))*(1*h2out*power(w8,(1-1))+0+0));
+//writeln(dEerror1{or 2}*(outo1r*(1-Outo1r))*w5);
+a:=dEerror1{or 2}*(outo1r*(1-Outo1r))*w5;
+b:=dEerror2{or 2}*(outo1r*(1-Outo1r))*w6;
+//writeln(a,' ',b);
+w1u:=((a+b)*h1out*(1-h1out)*0.05);
+w2u:=((a+b)*h1out*(1-h1out)*0.1);
+
+a:=dEerror1{or 2}*(outo1r*(1-Outo1r))*w7;
+b:=dEerror2{or 2}*(outo1r*(1-Outo1r))*w8;
+w3u:=((a+b)*h2out*(1-h2out)*0.05);
+w4u:=((a+b)*h2out*(1-h2out)*0.1);
+//writeln(w1-0.5*w1u,' ',w2-0.5*w2u,' ',w3-0.5*w3u,' ',w4-0.5*w4u);
+w1:=w1u;w2:=w2u;w3:=w3u;w4:=w4u;w5:=w5u;w6:=w6u;w7:=w7u;w8:=w8u;
+
+{h1net:=w1 * i1 + w2 * i2 + b1 * 1;
+h1out:=(logisticsFunction(h1net));
+
+h2net:=w3 * i1 + w4 * i2 + b1 * 1;
+h2out:=(logisticsFunction(h2net));
+
+o1r:=w5 * h1out + w6 * h2out + b2 * 1;
+Outo1r:=(logisticsFunction(o1r));
+
+o2r:=w7 * h1out + w8 * h2out + b2 * 1;
+Outo2r:=(logisticsFunction(o2r));}
+end;
 
 end.
