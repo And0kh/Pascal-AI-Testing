a3a81d6dbf1cfcfb2339e719e57d7fe76f588082
AussieMCer <andrei.okhrimouk@gmail.com>
2015-11-13 17:48:39 +0300

    Clean up
    Removed stuff that wasn't needed and added some comments to know what
    that part does.

--- Nueral network(1(2)input,1(2)hidden,1(2)output).pas
+++ Nueral network(1(2)input,1(2)hidden,1(2)output).pas
@@ -45,7 +45,6 @@ o2r:=w7 * h1out + w8 * h2out + b2 * 1;
 Outo2r:=(logisticsFunction(o2r));
 if(i=1) then begin or1ats:=Outo1r; or2ats:=Outo2r;end;
 
-//writeln(Outo1r,' ',Outo2r);
 
 //Error calculation
 dE1:=(sqr(outA1 - Outo1r)/2);
@@ -53,37 +52,26 @@ dE2:=(sqr(outA2 - Outo2r)/2);
 dEtotal:=dE1 + dE2;
 dEerror1:=(-(outA1 - Outo1r));
 dEerror2:=(-(outA2 - Outo2r));
+
+//Updating the weights of the second layer
 w5u:=w5-0.4*(dEerror1*(Outo1r*(1-Outo1r))*(1*h1out*power(w5,(1-1))+0+0));
-//writeln(w5u);
 w6u:=w6-0.4*(dEerror1*(Outo2r*(1-Outo2r))*(1*h2out*power(w6,(1-1))+0+0));
-//writeln(w6u);
 w7u:=w7-0.4*(dEerror2*(Outo2r*(1-Outo2r))*(1*h2out*power(w7,(1-1))+0+0));
 w8u:=w8-0.4*(dEerror2*(Outo2r*(1-Outo2r))*(1*h2out*power(w8,(1-1))+0+0));
-//writeln(dEerror1{or 2}*(outo1r*(1-Outo1r))*w5);
+
+//Updating the weights of the first layer
 a:=dEerror1{or 2}*(outo1r*(1-Outo1r))*w5;
 b:=dEerror2{or 2}*(outo1r*(1-Outo1r))*w6;
-//writeln(a,' ',b);
 w1u:=((a+b)*h1out*(1-h1out)*0.05);
 w2u:=((a+b)*h1out*(1-h1out)*0.1);
-
-a:=dEerror1{or 2}*(outo1r*(1-Outo1r))*w7;
-b:=dEerror2{or 2}*(outo1r*(1-Outo1r))*w8;
+a:=dEerror1*(outo1r*(1-Outo1r))*w7;
+b:=dEerror2*(outo1r*(1-Outo1r))*w8;
 w3u:=((a+b)*h2out*(1-h2out)*0.05);
 w4u:=((a+b)*h2out*(1-h2out)*0.1);
-//writeln(w1-0.5*w1u,' ',w2-0.5*w2u,' ',w3-0.5*w3u,' ',w4-0.5*w4u);
+
+//Assigning the variables the updated weights
 w1:=w1u;w2:=w2u;w3:=w3u;w4:=w4u;w5:=w5u;w6:=w6u;w7:=w7u;w8:=w8u;
 
-{h1net:=w1 * i1 + w2 * i2 + b1 * 1;
-h1out:=(logisticsFunction(h1net));
-
-h2net:=w3 * i1 + w4 * i2 + b1 * 1;
-h2out:=(logisticsFunction(h2net));
-
-o1r:=w5 * h1out + w6 * h2out + b2 * 1;
-Outo1r:=(logisticsFunction(o1r));
-
-o2r:=w7 * h1out + w8 * h2out + b2 * 1;
-Outo2r:=(logisticsFunction(o2r));}
 end;
 
 writeln('Output of the program on 1 loop- ',or1ats,' and ', or2ats);
